97c51dbdd66170db7fcd010685fe0f687cb1488d
PrincessPiggy22 <mlpuryear@my.waketech.edu>
2026-01-13 09:14:58 -0500

    Update exercise1_starter.java
    Created File Object, Append PrintWriter, generated/written 100 ints and closed the file

--- exercise1_starter.java
+++ exercise1_starter.java
@@ -2,36 +2,41 @@ import java.io.*;
 
 /**
  * Exercise 1: Write 100 random integers to a text file
- * 
+ *
  * Requirements:
  * - Create a file named Exercise17_01.txt if it does not exist
  * - If the file already exists, APPEND new data (don't overwrite)
  * - Write 100 integers created randomly (use Math.random())
  * - Integers should be separated by a space
  * - Use text I/O (PrintWriter)
- * 
+ *
  * @author Maddy Puryear
  */
-import java.io.File;       
-import java.io.IOException; 
+
 public class Exercise17_01 {
-    
+
     public static void main(String[] args) throws IOException {
         // TODO: Create a File object for "Exercise17_01.txt"
-        // File file = new File("Exercise17_01.txt");
-        
+        File file = new File("Exercise17_01.txt");
+
         // TODO: Create PrintWriter in APPEND mode
         // Hint: Use FileWriter with append parameter set to true
-        
-        
-        // TODO: Generate and write 100 random integers
-        // Hint: Use Math.random() to generate random integers
-        // Hint: Separate integers with a space
-        
-        
-        // TODO: Close the file (or use try-with-resources)
-        
-        
-        System.out.println("100 random integers written to Exercise17_01.txt");
+        try (PrintWriter output = new PrintWriter(new FileWriter(file, true))){
+
+            // TODO: Generate and write 100 random integers
+            // Hint: Use Math.random() to generate random integers
+            // Hint: Separate integers with a space
+            for(int i = 0; i < 100; i++){
+                int randint = (int)(Math.random() * (100 - 1 + 1))+ 1;
+                output.print(randint + " ");
+                if (i > 1 && i%20 == 0){
+                    output.print("\n");
+                }
+            }
+            // TODO: Close the file (or use try-with-resources)
+            output.close();
+            System.out.println("100 random integers written to Exercise17_01.txt");
+
+        }
     }
 }
